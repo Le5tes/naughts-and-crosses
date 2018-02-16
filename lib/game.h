165f0aa660d73c50c4a2f5d96cd1430bee60c032
Tim Williamson <timwilliamson1337@gmail.com>
2018-02-16 16:37:37 +0000

    lfs

--- lib/game.h
+++ lib/game.h
@@ -1,11 +1,46 @@
 #include "board.h"
+#include <iostream>
 class Game{
 
 private:
 	Board m_board;
+	char m_turn;
 
 public:
-	Game(){}
+	
+	void start(){
+		m_turn = 'O'; 
+		do {
+			int x = getInput();
+			m_board.setPosition(x/3, x%3, m_turn);
+			nextTurn();
+		} 
+		while(!m_board.winner());
+
+	}
+
+private:
+
+	int getInput(){
+		char a;
+		char b;
+		do {
+			std::cout << m_turn <<" take your go: ";
+			std::cin >> a;
+			std::cin >>b;
+			std::cin.ignore(32767, '\n');
+		} while ((a == 'a' or a == 'b' or a == 'c') and (b == '1' or b == '2' or b == '3'));
+		int output = (a - 96) * 3 + (b-49); //converting chars to numbers
+		return output;
+	}
+
+	char nextTurn(){
+		if (m_turn == 'O'){
+			m_turn = 'X';
+		} else {
+			m_turn = 'O';
+		}
+	}
 		
 	
 };
--- lib/noughts-and-crosses.cpp
+++ lib/noughts-and-crosses.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include "game.h"
-// void output(char board[3][3]){
-// 	std::cout << "  a b c\n";
-// 	for(int i = 0; i < 3; i++){
-// 		std::cout << i << " ";
-// 		for(int j = 0; j < 3; j++){
-// 			if (board[i][j]){
-// 				std::cout << board[i][j];
-// 			} else {
-// 				std::cout << " ";
-// 			}
-// 			if (j<2){std::cout << "|";}
-// 		}
-// 		std::cout << "\n"; 
-// 		if (i<2){ std::cout << "  -+-+-" <<"\n"; }
-// 	}
-	
-// }
-int main(){
-	// char myBoard[3][3] = {" "};
-	// myBoard[0][2] = 88;
-	// myBoard[2][0] = 79;
+void openingMessages(){
+	std::cout << "Welcome to noughts and crosses! \n ";
+	std::cout << "Noughts goes first as you well know. \n";
+}
 
-	Board myBoard;
+
+int main(){
+
+	openingMessages();
+	std::string startGame;
+	do {
+		Game game;
+		game.start();
+		do{
+			std::cout << "Do you want to play again? y/n";
+			std::cin >> startGame;
+		} 
+		while (startGame == "y" or startGame == "n");
+	} 
+	while (startGame == "y"); 
+
+	// Board myBoard;
 	
-	myBoard.setPosition(0,2,88);
-	myBoard.setPosition(2,0,79);
-	myBoard.print();
+	// myBoard.setPosition(0,2,88);
+	// myBoard.setPosition(2,0,79);
+	// myBoard.print();
 }
